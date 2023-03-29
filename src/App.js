b33f187aa8409b02db1d20238f618628c50edd1a
martadudka <martakryklevych@gmail.com>
2023-03-29 08:44:18 +0200

    added github repository URL

--- src/App.js
+++ src/App.js
@@ -2,6 +2,23 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">Weather App</div>
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "} 
+        <a 
+        href="https://martadudka.netlify.app/"
+        target="_blank"
+        >
+          Marta Dudka
+          </a> and is{" "}
+        <a
+          href="https://github.com/martadudka/weather-app-react"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
   );
 }
