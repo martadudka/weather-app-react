1e74389eead3cc939abfd29c539f2405e8e2569a
martadudka <martakryklevych@gmail.com>
2023-04-05 14:19:39 +0200

    added api integration

--- src/App.js
+++ src/App.js
@@ -5,7 +5,25 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Rome" />
+        <footer className="Footer-Link">
+          This project was coded by{" "}
+          <a
+            href="https://martadudka.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Marta Dudka
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/martadudka/weather-app-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,93 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-    return (
-      <div class="weather-app">
-        <form id="search-form" class="search mb-3">
-          <div class="row">
-            <div class="col-5">
-              <input
-                type="search"
-                id="city-search-input"
-                placeholder="Enter a city"
-                autocomplete="off"
-                class="form-control"
-              />
-            </div>
-            <div class="col">
-              <input
-                type="submit"
-                value="Search"
-                class="btn btn-primary"
-                id="search-button-input"
-              />
-            </div>
-            <div class="col">
-              <button
-                class="btn btn-success current-location-button"
-                id="current-location-button"
-              >
-                Current location
-              </button>
-            </div>
-          </div>
-        </form>
-        <div class="row info">
-          <div class="col">
-            <strong class="city-name" id="city-name">
-              Rome
-            </strong>
-            <ul class="current-date">
-              <li>
-                Last updated: <div id="date">Wednesday, 14:20</div>
-              </li>
-            </ul>
-          </div>
-          <div class="col current-temperature">
-            <img
-              src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
-              alt="clear sky"
-              id="current-weather-icon"
-            />
-            <span class="degrees" id="degrees">
-              20
-            </span>
-            <span class="celsius">°C</span>
-          </div>
-          <div class="col">
-            <strong class="weather-description" id="weather-description">
-              Clear sky
-            </strong>
-            <ul class="current-weather-description">
-              <li class="humidity">
-                Humidity: <span id="humidity">12</span>%
-              </li>
-              <li class="wind">
-                Wind: <span id="wind">1</span> km/h
-              </li>
-            </ul>
-          </div>
-        </div>
+export default function Weather(props) {
+    const [weatherData, setWeatherData] = useState({ready: false});
+    function handleResponse(response) {
+        setWeatherData({
+            ready: true,
+            temperature: response.data.temperature.current,
+            humidity: response.data.temperature.humidity,
+            wind: response.data.wind.speed,
+            city: response.data.city,
+            date: "Wednesday 14:20",
+            iconUrl: response.data.condition.icon_url,
+            description: response.data.condition.description
+        });
+    }
 
-        <footer className="Footer-Link">
-          This project was coded by{" "}
-          <a
-            href="https://martadudka.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Marta Dudka
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://github.com/martadudka/weather-app-react"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            open-sourced on GitHub
-          </a>
-        </footer>
-      </div>
-    );
+    if (weatherData.ready) {
+        return (
+          <div className="weather-app">
+            <form id="search-form" className="search mb-3">
+              <div className="row">
+                <div className="col-5">
+                  <input
+                    type="search"
+                    id="city-search-input"
+                    placeholder="Enter a city"
+                    autocomplete="off"
+                    className="form-control"
+                  />
+                </div>
+                <div className="col">
+                  <input
+                    type="submit"
+                    value="Search"
+                    className="btn btn-primary"
+                    id="search-button-input"
+                  />
+                </div>
+                <div className="col">
+                  <button
+                    className="btn btn-success current-location-button"
+                    id="current-location-button"
+                  >
+                    Current location
+                  </button>
+                </div>
+              </div>
+            </form>
+
+            <div className="row info">
+              <div className="col">
+                <strong className="city-name" id="city-name">
+                  {weatherData.city}
+                </strong>
+                <ul className="current-date">
+                  <li>
+                    Last updated: <div id="date">{weatherData.date}</div>
+                  </li>
+                </ul>
+              </div>
+              <div className="col current-temperature">
+                <img
+                  src={weatherData.iconUrl}
+                  alt={weatherData.description}
+                  id="current-weather-icon"
+                />
+                <span className="degrees" id="degrees">
+                  {Math.round(weatherData.temperature)}
+                </span>
+                <span className="celsius">°C</span>
+              </div>
+              <div className="col">
+                <strong
+                  className="weather-description"
+                  id="weather-description"
+                >
+                  {weatherData.description}
+                </strong>
+                <ul className="current-weather-description">
+                  <li className="humidity">
+                    Humidity: <span id="humidity">{weatherData.humidity}</span>%
+                  </li>
+                  <li className="wind">
+                    Wind: <span id="wind">{weatherData.wind}</span> km/h
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        );
+    }else{
+    const apiKey = "1d1a4ta9d508e3cb925a520dd24fdc3o";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading..."
+    }
 }
